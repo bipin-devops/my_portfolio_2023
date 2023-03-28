3ce09389a3e52a3f625f898d3465796ac81b79d8
Bipin Raj Sitoula <viraatsitoula@gmail.com>
2023-03-28 16:47:54 +1100

    Social Media Icons updated

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,18 +1,33 @@
 import React from "react";
-import { BsTwitter, BsInstagram } from "react-icons/bs";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
 import { FaFacebookF } from "react-icons/fa";
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
-      <div>
-        <BsTwitter />
+      <div style={{ cursor: "pointer" }}>
+        <BsLinkedin
+          onClick={() =>
+            window.open(
+              "https://www.linkedin.com/in/bipin-raj-sitoula/",
+              "_blank"
+            )
+          }
+        />
       </div>
-      <div>
-        <FaFacebookF />
+      <div style={{ cursor: "pointer" }}>
+        <FaFacebookF
+          onClick={() =>
+            window.open("https://www.facebook.com/bpnsitoula21/", "_blank")
+          }
+        />
       </div>
-      <div>
-        <BsInstagram />
+      <div style={{ cursor: "pointer" }}>
+        <BsGithub
+          onClick={() =>
+            window.open("https://github.com/bipin-devops", "_blank")
+          }
+        />
       </div>
     </div>
   );
